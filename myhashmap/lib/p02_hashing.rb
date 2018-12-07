4675be5e065debe816b1c28e086f1c6e917bfb1d
Tae Hui <jasmine878@yahoo.com>
2018-12-07 17:30:43 -0500

    Updated myhashmap

--- myhashmap/lib/p02_hashing.rb
+++ myhashmap/lib/p02_hashing.rb
@@ -1,19 +1,37 @@
+require 'byebug'
+
 class Fixnum
   # Fixnum#hash already implemented for you
 end
 
 class Array
+  #NOT a normal hash.  This is a hashmap.
+  #we use a hash to come up with unique combinations to represent an Array
   def hash
     hash_result = ""
     self.each_with_index do |ele, idx|
-      hash_result << ele * (idx + 1)
+      if ele.is_a?(Symbol)
+        hash_result << (ele.to_s.ord * (idx + 1)).to_s
+      elsif !ele.is_a?(Integer)
+        hash_result << (ele.ord * (idx + 1)).to_s
+      else
+        hash_result << (ele * (idx + 1)).to_s
+      end
     end
     hash_result.to_i
   end
 end
 
 class String
+  #NOT a normal hash.  This is a hashmap.
+  #we use a hash to come up with unique combinations to represent an String
   def hash
+    alphabet = ("a".."z").to_a + ("A".."Z").to_a
+    arr = []
+    self.each_char.with_index do |letter, idx|
+      arr << alphabet.index(letter)
+    end
+    arr.hash
   end
 end
 
@@ -21,6 +39,6 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    self.to_a.sort.flatten.hash
   end
 end
--- myhashmap/lib/p03_hash_set.rb
+++ myhashmap/lib/p03_hash_set.rb
@@ -7,18 +7,34 @@ class HashSet
   end
 
   def insert(key)
+    resize! if @count == num_buckets
+
+    #.hash converts key to a unique combination of numbers
+    #.hash is a unique identifier
+    #But we shovel in the key into the bucket location, not the unique identifier
+      unless self.include?(key.hash)
+        self[key.hash] << key
+        @count += 1
+      end
   end
 
   def include?(key)
+    self[key.hash].include?(key)
   end
 
   def remove(key)
+    if self.include?(key)
+      self[key.hash].delete(key)
+      @count -= 1
+    end
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    i = num % @store.length
+    @store[i]
   end
 
   def num_buckets
@@ -26,5 +42,13 @@ class HashSet
   end
 
   def resize!
+    new_store = Array.new(num_buckets * 2) { Array.new }
+
+    @store.each do |sub_arr|
+      sub_arr.each do |key|
+        new_store[key.hash % new_store.length] << key
+      end
+    end
+      @store = new_store
   end
 end
